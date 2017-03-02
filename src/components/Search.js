85dc7850ad0ec39d19d2beeccdffc7b515a8ce2a
Paweł Szymański <peal1982@gmail.com>
2017-03-02 16:26:33 +0100

    feature/FD6K8-28_Search _Example  Wyswietl wyniki wyszukania

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import db from
-import Filter_music from './search_functions/Filter_band'
-import Filter_band from './search_functions/Filter_band'
-import Filter_date from './search_functions/Filter_music'
+import filter_music from './search_functions/Filter_music'
+import filter_band from './search_functions/Filter_band'
+import filter_date from './search_functions/Filter_music'
 
 
 const Search = (db) => {
@@ -12,3 +11,5 @@ const Search = (db) => {
     .filter( filter_band )
     .filter( filter_date )
 }
+
+export default Search
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -2,9 +2,32 @@ import React from 'react';
 import Search from './Search'
 
 
-const SearchView = () => (
-  Search(db)
-)
+const db =[
+  {
+  zespol: "metalica",
+  data: "13.07.2014"
+  },
+  {
+    zespol: "Slayer",
+    data: "18.09.2014"
+  }
+]
+
+var searchResult = Search(db);
+
+
+const SearchView = () => {
+  return (
+    <div>
+      <div>Tu bedzie wyszukiwarka</div>
+      <div>Wyniki Wyszukania</div>
+      {
+        searchResult
+          .map( search => (<div>{search.zespol}</div>) )
+      }
+    </div>
+  )
+}
 
 export default SearchView;
 
