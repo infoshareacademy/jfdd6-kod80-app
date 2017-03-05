6968749014a03f63337dd0cd2e781ae52fca6bbe
PawelSzy <peal1982@gmail.com>
2017-03-05 18:27:37 +0100

    feature/FD6K8-28_search_functional_way Stworzenie szkieletu wyszukiwania koncertow w stylu funkcyjnym filtrowanie muzyka, data, zespol

--- src/search/search_functions/filter-band.js
+++ src/search/search_functions/filter-band.js
@@ -1,8 +1,19 @@
-const Filter_band= (concert) => {
-  //if ( warunek(concert) ===  true)
+
+// Funkcja, ktora zwraca funkcje, ktora sprawdza czy koncert
+// spełnia warunki
+// tutaj ***nazwa zespolu***
+// @param - search values - warunki wyszukiwania
+// @ return - funkcja
+//        zwracana funkcja - sprawdza czy koncert spełnia warunke
+//        zwracana funkcja - @param - koncert
+//        zwracana funkcja - @return - Boolean - True jesli koncert spełnia warunek, False jesli nie spełnia
+const filter_band= searchValues => concert => {
   return true
-  //else
-  //return false
+  //if( concert.TypeOfMusic === "Metal") {
+  //  return true
+  //} else {
+  //  return false
+  //}
 }
 
-export default Filter_band
+export default filter_band
