fd4ee3d01ed97d95cba09018c1596a858b643fb9
PawelSzy <peal1982@gmail.com>
2017-03-05 16:12:18 +0100

    bugfix/porzadki change name to small cases - filter functions

--- src/search/search.js
+++ src/search/search.js
@@ -1,7 +1,7 @@
 
-import filter_music from './search_functions/Filter_music'
-import filter_band from './search_functions/Filter_band'
-import filter_date from './search_functions/Filter_date'
+import filter_music from './search_functions/filter-music.js'
+import filter_band from './search_functions/filter-band.js'
+import filter_date from './search_functions/filter-date.js'
 
 const search = (database, searchValues) => {
   return database
