9f6e7811a7298a4df4c5b63d061ebff79716fa72
Paweł Szymański <peal1982@gmail.com>
2017-03-06 17:06:30 +0100

    develop/zmiana_nazw Odczytaj dane koncertu pierwsze kroki

--- src/components/concert-card-view.js
+++ src/components/concert-card-view.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Attractions from './attractions-view'
+import search from '../search/search'
+import SearchRowResult from './search-view-row-result'
 
-const ConcertCard = props => (
-  <div>
-    <h1>props.</h1> {props.params.concertId}
+import database from '../data/data-testowa-baza-danych.json'
 
-    <Attractions />
-  </div>
-)
+const ConcertCard = props => {
+  const concertId = props.params.concertId
+  console.log(concertId)
+  const searchValues = {
+    concert_id: concertId,
+    date: "",
+    places: "",
+    music: "",
+    concert: ""
+  }
+
+  const searchResult = search(database, searchValues);
+  return (
+    <div>
+      <h1>props.</h1>
+      <ul>
+        {
+          searchResult
+            .map( concert => <SearchRowResult key={concert.id} concert={concert} /> )
+        }
+      </ul>
+
+      <Attractions />
+    </div>
+  )
+}
 
 export default ConcertCard;
