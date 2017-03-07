b53e818bb336237c14c35a2b054080a788fb9658
Paweł Szymański <peal1982@gmail.com>
2017-03-07 16:03:31 +0100

    develop/zmiana_nazw Url do karty koncertu

--- src/components/concert-card-view.js
+++ src/components/concert-card-view.js
@@ -7,29 +7,17 @@ import database from '../data/data-testowa-baza-danych.json'
 
 const ConcertCard = props => {
   const concertId = props.params.concertId
-  console.log(concertId)
-  const searchValues = {
-    concert_id: concertId,
-    date: "",
-    places: "",
-    music: "",
-    concert: ""
-  }
 
-  const searchResult = search(database, searchValues);
   return (
-    <div>
-      <h1>props.</h1>
-      <ul>
-        {
-          searchResult
-            .map( concert => <SearchRowResult key={concert.id} concert={concert} /> )
-        }
-      </ul>
-
+      <div>
+        <h1>Stronaaaaaaaaaaaaaaaaaa Koncertu Do Zrobienia</h1>
       <Attractions />
     </div>
   )
 }
 
 export default ConcertCard;
+
+
+
+
