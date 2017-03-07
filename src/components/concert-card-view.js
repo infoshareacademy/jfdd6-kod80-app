8ff80a64c1f743663b15247d3b01ace1b9d7247d
PawelSzy <peal1982@gmail.com>
2017-03-07 20:10:31 +0100

    feature/FD6K8-33_Strona_koncertu Wyswietl podstawowe infromacje o koncercie i dodaj button

--- src/components/concert-card-view.js
+++ src/components/concert-card-view.js
@@ -3,17 +3,23 @@ import Attractions from './attractions-view'
 import search from '../search/search'
 import SearchRowResult from './search-view-row-result'
 
+import {Button} from 'react-bootstrap'
+
 import database from '../data/data-testowa-baza-danych.json'
 
 const ConcertCard = props => {
   const concertId = props.params.concertId
-  const concert = database.filter( concert => concert.id == concertId)
+  const concert = database.filter( concert => concert.id == concertId)[0]
+
+  console.log(concert)
 
   return (
       <div key={concertId}>
 
-        <h1 >concert</h1>
-        <h2> koncertId: {concertId} </h2>
+        <h1 >{concert.Band}</h1>
+        <h2> {concert.City} {concert.Date} </h2>
+
+        <Button  bsStyle="danger" bsSize="large">Kup</Button>
 
       <Attractions />
     </div>
