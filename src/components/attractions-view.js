d46eebb82c5e67c326892b1cb38a3f0eaddc96e4
Marek Kita <kitamarekweb@gmail.com>
2017-03-08 20:58:26 +0100

    change attractions-view from 'const' to 'class'

--- src/components/attractions-view.js
+++ src/components/attractions-view.js
@@ -23,143 +23,152 @@ const calculateDistance = (latA, lonA, latB, lonB) => {
 
 }
 
-const AttractionsView = () => (
-  <Grid>
 
-    <h1>Explore Gdańsk</h1>
-    <div className="row">
-      <div className="col-xs-3">
-        <h1>Attractions</h1>
-        <Table striped>
-          <thead>
-          <tr>
-            <th>Id</th>
-            <th>Attraction</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            attractions.map(
-              attraction => (
-                <tr key={attraction.id}>
-                  <td>{attraction.id}</td>
-                  <td>
-                    <ul>
-                      <li>{attraction.name}</li>
-                      <li>{attraction.address}</li>
-                      <li>{attraction.hours}</li>
-                      <li>{attraction.website !== 'no website' ?
-                        <a href={attraction.website}>Website</a> : 'no website'}</li>
-                      <li>{calculateDistance(attraction.latitude, attraction.longitude, concertX, concertY).toFixed(2)}
-                        km
-                      </li>
-                    </ul>
-                  </td>
-                </tr>
-              )
-            )
-          }
-          </tbody>
-        </Table>
-      </div>
+class AttractionsView extends React.Component {
 
-      <div className="col-xs-3">
-        <h1>Restaurants</h1>
-        <Table striped>
-          <thead>
-          <tr>
-            <th>Id</th>
-            <th>Restaurant</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            restaurants.map(
-              restaurant => (
-                <tr key={restaurant.id}>
-                  <td>{restaurant.id}</td>
-                  <td>
-                    <ul>
-                      <li>{restaurant.name}</li>
-                      <li>{restaurant.address}</li>
-                      <li>{restaurant.hours}</li>
-                      <li>{restaurant.phone}</li>
-                      <li>{restaurant.website !== 'no website' ?
-                        <a href={restaurant.website}>Website</a> : 'no website'}</li>
-                    </ul>
-                  </td>
-                </tr>
-              )
-            )
-          }
-          </tbody>
-        </Table>
-      </div>
+  render () {
+    // const ...
 
-      <div className="col-xs-3">
-        <h1>Hotels</h1>
-        <Table striped>
-          <thead>
-          <tr>
-            <th>Id</th>
-            <th>Hotel</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            hotels.map(
-              hotel => (
-                <tr key={hotel.id}>
-                  <td>{hotel.id}</td>
-                  <td>
-                    <ul>
-                      <li>{hotel.name}</li>
-                      <li>{hotel.address}</li>
-                      <li>{hotel.phone}</li>
-                      <li>{hotel.website !== 'no website' ? <a href={hotel.website}>Website</a> : 'no website'}</li>
-                    </ul>
-                  </td>
-                </tr>
-              )
-            )
-          }
-          </tbody>
-        </Table>
-      </div>
+    return (
+      <Grid>
 
-      <div className="col-xs-3">
-        <h1>Taxis</h1>
-        <Table striped>
-          <thead>
-          <tr>
-            <th>Id</th>
-            <th>Taxi</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            taxis.map(
-              taxi => (
-                <tr key={taxi.id}>
-                  <td>{taxi.id}</td>
-                  <td>
-                    <ul>
-                      <li>{taxi.name}</li>
-                      <li>{taxi.phone}</li>
-                    </ul>
-                  </td>
-                </tr>
-              )
-            )
-          }
-          </tbody>
-        </Table>
-      </div>
+        <h1>Explore Gdańsk</h1>
+        <div className="row">
+          <div className="col-xs-3">
+            <h1>Attractions</h1>
+            <Table striped>
+              <thead>
+              <tr>
+                <th>Id</th>
+                <th>Attraction</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                attractions.map(
+                  attraction => (
+                    <tr key={attraction.id}>
+                      <td>{attraction.id}</td>
+                      <td>
+                        <ul>
+                          <li>{attraction.name}</li>
+                          <li>{attraction.address}</li>
+                          <li>{attraction.hours}</li>
+                          <li>{attraction.website !== 'no website' ?
+                            <a href={attraction.website}>Website</a> : 'no website'}</li>
+                          <li>{calculateDistance(attraction.latitude, attraction.longitude, concertX, concertY).toFixed(2)}
+                            km
+                          </li>
+                        </ul>
+                      </td>
+                    </tr>
+                  )
+                )
+              }
+              </tbody>
+            </Table>
+          </div>
+
+          <div className="col-xs-3">
+            <h1>Restaurants</h1>
+            <Table striped>
+              <thead>
+              <tr>
+                <th>Id</th>
+                <th>Restaurant</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                restaurants.map(
+                  restaurant => (
+                    <tr key={restaurant.id}>
+                      <td>{restaurant.id}</td>
+                      <td>
+                        <ul>
+                          <li>{restaurant.name}</li>
+                          <li>{restaurant.address}</li>
+                          <li>{restaurant.hours}</li>
+                          <li>{restaurant.phone}</li>
+                          <li>{restaurant.website !== 'no website' ?
+                            <a href={restaurant.website}>Website</a> : 'no website'}</li>
+                        </ul>
+                      </td>
+                    </tr>
+                  )
+                )
+              }
+              </tbody>
+            </Table>
+          </div>
+
+          <div className="col-xs-3">
+            <h1>Hotels</h1>
+            <Table striped>
+              <thead>
+              <tr>
+                <th>Id</th>
+                <th>Hotel</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                hotels.map(
+                  hotel => (
+                    <tr key={hotel.id}>
+                      <td>{hotel.id}</td>
+                      <td>
+                        <ul>
+                          <li>{hotel.name}</li>
+                          <li>{hotel.address}</li>
+                          <li>{hotel.phone}</li>
+                          <li>{hotel.website !== 'no website' ? <a href={hotel.website}>Website</a> : 'no website'}</li>
+                        </ul>
+                      </td>
+                    </tr>
+                  )
+                )
+              }
+              </tbody>
+            </Table>
+          </div>
+
+          <div className="col-xs-3">
+            <h1>Taxis</h1>
+            <Table striped>
+              <thead>
+              <tr>
+                <th>Id</th>
+                <th>Taxi</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                taxis.map(
+                  taxi => (
+                    <tr key={taxi.id}>
+                      <td>{taxi.id}</td>
+                      <td>
+                        <ul>
+                          <li>{taxi.name}</li>
+                          <li>{taxi.phone}</li>
+                        </ul>
+                      </td>
+                    </tr>
+                  )
+                )
+              }
+              </tbody>
+            </Table>
+          </div>
 
 
-    </div>
-  </Grid>
-)
+        </div>
+      </Grid>
+    )
+  }
+}
+
 export default AttractionsView;
 
 
