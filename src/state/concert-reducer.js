da7e7e463c4516dda72d8283566b070d09d704ec
Marek Kita <kitamarekweb@gmail.com>
2017-03-09 22:28:24 +0100

    ... zmiana podejscia...

--- src/state/concert-reducer.js
+++ src/state/concert-reducer.js
@@ -1,7 +1,24 @@
+const GET_CONCERT = 'concert/GET_CONCERTS'
+
+export const getConcert = concert => {(
+  type: GET_CONCERT
+  concert
+)}
+
+
+
 const initialState = {
   dataConcert: null
 }
 
 export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    case GET_CONCERT:
+      return{
+        ...state,
+        dataConcert: state
+      }
+    default:
       return state
   }
+}
