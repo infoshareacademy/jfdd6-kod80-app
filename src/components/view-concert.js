bd210049e311da1a502cc32a3807a231460ccda4
Marek Kita <kitamarekweb@gmail.com>
2017-03-10 07:04:47 +0100

    wykomentowanie tego z czego zrezygnowalem

--- src/components/view-concert.js
+++ src/components/view-concert.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router'
-import { connect } from 'react-redux'
-import { Grid } from 'react-bootstrap'
-
-
-const ViewConcert
-
-
-export default connect (
-  state => ({
-
-  })
-)
+// import React from 'react'
+// import { Link } from 'react-router'
+// import { connect } from 'react-redux'
+// import { Grid } from 'react-bootstrap'
+//
+//
+// const ViewConcert
+//
+//
+// export default connect (
+//   state => ({
+//
+//   })
+// )
--- src/store.js
+++ src/store.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers } from 'redux'
 
 import searchReducer from './state/search-value'
 import distanceReducer from './state/distance-filter'
-import concertReducer from './state/concert-reducer'
+// import concertReducer from './state/concert-reducer'
 
 const reducer = combineReducers({
   searchValues: searchReducer,
   distanceVal: distanceReducer,
-  concerts: concertReducer
+  // concerts: concertReducer
 })
 
 const store = createStore(
