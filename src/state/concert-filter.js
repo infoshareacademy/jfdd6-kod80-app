0f5cb77e20b3b6b996a3a01446c0b532cc89603d
Pawel Szymański <your_email@example.com>
2017-03-12 00:05:42 +0100

    Ładniejszy kod w concert-filters3 Opis funkcji do dispatchowania

--- src/state/concert-filter.js
+++ src/state/concert-filter.js
@@ -1,5 +1,13 @@
 // ACTION TYPES (there may be more than one)
+
+
 const SET_TYPE_OF_MUSIC_SEARCH = 'concerts/SET_TYPE_OF_MUSIC_SEARCH'
+
+//setTypeOfMusicSearch np.: setTypeOfMusicSearch('Pop')
+//setDateSearch np.: setDateSearch(date)
+//setCitySearch np.: setCitySearch('Gdańsk')
+//setConcertIdSearch np.: setConcertIdSearch(1)
+//setBandNameSearch np.: setBandNameSearch('Queen')
 
 export const setTypeOfMusicSearch = (typeOfMusic, valueType) => ({
   type: SET_TYPE_OF_MUSIC_SEARCH,
