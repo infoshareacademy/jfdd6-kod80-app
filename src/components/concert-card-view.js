139a384750b52d7de8611e888ef296ab2816d1c8
Agata Cała <agata.cala@gmail.com>
2017-03-13 15:39:07 +0100

    Add concert-card-view to store - ready for PullRequest

--- src/components/concert-card-view.js
+++ src/components/concert-card-view.js
@@ -1,89 +1,50 @@
 import React from 'react';
 import Attractions from './attractions-view'
 import {Button, Grid} from 'react-bootstrap'
-
 import {connect} from 'react-redux'
 import {fetchSuccess} from '../state/concertCardViewReducer'
 
-
 export default connect(
   state => ({
-      database: state.database
+    database: state.database
   }),
   dispatch => ({
-      fetchDatabase: () => dispatch(fetchSuccess())
+    fetchDatabase: () => dispatch(fetchSuccess())
   })
 )(
   class ConcertCard extends React.Component {
-
-      componentWillMount() {
-          this.props.fetchDatabase()
-      }
-       render() {
-          return (
-              <Grid>
-                  <div>
-                      <h1>karta wybranego koncertu</h1>
-                      {
-
-
-
-                          this.props.database.data !== null ? this.props.database.data.map(
-                                  (concert, index) => <li key={index}>{concert.band}</li>
-                              ) : <p>Wczytywanie bazy danych...</p>
-
-
-                      }
-                  </div>
-                  <div>
-                      <a href="https://www.ebilet.pl">
-                          <Button bsStyle="danger" bsSize="large">Kup Bilet !</Button>
-                      </a>
-
-                      <Attractions />
-                  </div>
-              </Grid>
-          )
-      }
+    componentWillMount() {
+      this.props.fetchDatabase()
+    }
+    render() {
+      return (
+        <Grid>
+          <div>
+            <h1>karta wybranego koncertu</h1>
+            {
+              this.props.database.data !== null ?
+                this.props.database.data.filter(concert => concert.id === parseInt(this.props.params.concertId)
+                )
+                  .map(
+                    (concert, index) =>
+                      <div key={index}>
+                        <h1>{concert.band}</h1>
+                        <h4>{concert.city} </h4>
+                        <h4> {concert.date}</h4>
+                        <h4> {concert.ticketPrice}</h4>
+                      </div>
+                  )
+                : <p>Wczytywanie bazy danych...</p>
+            }
+          </div>
+          <div>
+            <a href="https://www.ebilet.pl">
+              <Button bsStyle="danger" bsSize="large">Kup Bilet !</Button>
+            </a>
+            <Attractions />
+          </div>
+        </Grid>
+      )
+    }
   }
 )
-
-
-//
-// import React from 'react';
-// import Attractions from './attractions-view'
-// import {Button, Grid} from 'react-bootstrap'
-// import {LinkContainer} from 'react-router-bootstrap'
-// import { connect } from 'react-redux'
-//
-// import database from '../../public/data/concerts-gdansk.json'
-//
-//
-// class ConcertCard extends React.Component {
-//
-//
-//
-//
-// }
-//
-// const ConcertCard = props => {
-//     const concertId = props.params.concertId;
-//     const concert = database.filter( concert => concert.id == concertId)[0];
-//
-//     return (
-//         <Grid key={concertId}>
-//
-//             <h1 >{concert.band}</h1>
-//             <h2> {concert.city} {concert.date} </h2>
-//             <LinkContainer to={{ pathname: '/buy-ticket/3' }}>
-//                 <Button  bsStyle="danger" bsSize="large">Kup Bilet !</Button>
-//             </LinkContainer>
-//
-//             <Attractions />
-//         </Grid>
-//     )
-// };
-//
-//
-// export default ConcertCard
-
