9177d5a2eee797eda4efeede68d95916a166c742
Agata Cała <agata.cala@gmail.com>
2017-03-13 16:03:49 +0100

    Add concert-card-view to store - ready for PullRequest

--- src/components/concert-card-view.js
+++ src/components/concert-card-view.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Attractions from './attractions-view'
-import search from '../search/search'
-import SearchRowResult from './search-view-row-result'
+
 
 import {Button, Grid} from 'react-bootstrap'
 import {connect} from 'react-redux'
