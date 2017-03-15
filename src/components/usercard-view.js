b56728145ea5ebdd3ff307f572583cf680caea53
Karolina Kreft <kar.kreft@gmail.com>
2017-03-15 16:24:20 +0100

    Changed the view to previous one, without a search input

--- src/components/usercard-view.js
+++ src/components/usercard-view.js
@@ -2,26 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
 import {Grid, Alert, Table} from 'react-bootstrap'
-import SearchInputCreator from './search-input'
 
-import filter_concert from '../search/concert-filter'
 
 export default connect(
   state => ({
-    concerts: state.concerts,
-    concertsSearchValues: state.concert_filter.concertsSearchValues
+    concerts: state.concerts
   })
 )(
   class UsercardView extends React.Component {
     render() {
-      const {
-        concerts,
-        concertsSearchValues
-      } = this.props
+      const {concerts} = this.props
 
       return (
         <Grid>
-          <SearchInputCreator/>
           <h1>Dostępne koncerty</h1>
           {
             concerts.fetching ?
@@ -39,31 +32,25 @@ export default connect(
           <Table striped>
             <thead>
             <tr>
-              <th>Id</th>
               <th>Zespół</th>
               <th>Typ muzyki</th>
-              <th>Miasto</th>
               <th>Data</th>
             </tr>
             </thead>
             <tbody>
             {
               concerts.data ?
-                concerts.data
-                  .filter( filter_concert(concertsSearchValues) )
-                  .map(
-                    concert => (
-                      <tr key={concert.id}>
-                        <td>{concert.id}</td>
-                        <td>
-                          <Link to={'/koncerty/' + concert.id}>{concert.band}</Link>
-                        </td>
-                        <td>{concert.typeOfMusic}</td>
-                        <td>{concert.city}</td>
-                        <td>{concert.date}</td>
-                      </tr>
-                    )
-                  ) : null
+                concerts.data.map(
+                  concert => (
+                    <tr key={concert.id}>
+                      <td>
+                        <Link to={'/koncerty/' + concert.id}>{concert.band}</Link>
+                      </td>
+                      <td>{concert.typeOfMusic}</td>
+                      <td>{concert.date}</td>
+                    </tr>
+                  )
+                ) : null
             }
             </tbody>
           </Table>
