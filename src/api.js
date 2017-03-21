d49099a4ca5f58591d78f217fb0fb2cb141d9621
Karolina Kreft <kar.kreft@gmail.com>
2017-03-21 15:45:31 +0100

    Added api file with url linking to production server? :D

---
+++ src/api.js
@@ -0,0 +1,17 @@
+let url = 'http://localhost:3010/api'
+
+if (process.env.NODE_ENV === "production") {
+  url = 'https://radiant-mountain-66074.herokuapp.com/api'
+}
+
+export default { url }
+
+
+
+
+
+
+
+
+
+
