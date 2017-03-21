5ebb8bd825a8a82c51bf852bd89b03e8bad90b37
Marek Kita <kitamarekweb@gmail.com>
2017-03-21 16:03:57 +0100

    attend reducer + api

--- src/state/attend-concert.js
+++ src/state/attend-concert.js
@@ -1,10 +1,29 @@
+import Api from '../api'
+
 const ATTEND_CONCERT = 'attend-concert/ATTEND_CONCERT'
 const LEAVE_CONCERT = 'leave-concert/LEAVE_CONCERT'
 
-export const attendConcert = concertId => ({
-  type: ATTEND_CONCERT,
-  concertId
-})
+
+export const attendConcert = (concertId, userId, accessToken) => dispatch => fetch(
+  Api.url + userId + '/favoriteItems?access_token=' + accessToken, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      itemId: concertId,
+      itemType: 'concert',
+    })
+  }
+).then(
+  response => response.json()
+).then(
+  data => dispatch({
+    type: ATTEND_CONCERT,
+    concertId,
+    favId: data.id
+  })
+)
 
 export const leaveConcert = concertId => ({
   type: LEAVE_CONCERT,
