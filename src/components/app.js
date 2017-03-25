abad42041030b76983b3787a8c6ea6f6020400de
Pawel Szymański <your_email@example.com>
2017-03-25 15:30:10 +0100

    bledy logowania naprawa proba

--- src/components/app.js
+++ src/components/app.js
@@ -91,8 +91,8 @@ class App extends React.Component {
                 <LinkContainer to="/moje-konto">
                   <NavItem href="#">Moje konto</NavItem>
                 </LinkContainer>
-                <LinkContainer to="/moje-konto"  onClick={() => this.props.logOut()}>
-                  <NavItem href="#">Wyloguj</NavItem>
+                <LinkContainer to="/"  onClick={() => this.props.logOut()}>
+                  <NavItem>Wyloguj</NavItem>
                 </LinkContainer>
               </Nav>
             </Navbar.Collapse>
